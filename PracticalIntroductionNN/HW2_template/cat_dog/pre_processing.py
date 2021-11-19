981b8e07ab1be8f62c37d6c46fe8ec17d74f66b5
alberthu233 <60637412+alberthu233@users.noreply.github.com>
2021-11-19 15:36:40 -0800

    alex

--- PracticalIntroductionNN/HW2_template/cat_dog/pre_processing.py
+++ PracticalIntroductionNN/HW2_template/cat_dog/pre_processing.py
@@ -11,9 +11,16 @@ test_dir = "PracticalIntroductionNN\HW2_template\cat_dog\\test1\\"
 train_img, train_labels = [], []
 test_img = []
 
-f = h5py.File("D:\Work\EPE\PracticalIntroductionNN\HW2_template\cat_dog\\dc_227.h5", "w")
+f = h5py.File("D:\Work\EPE\PracticalIntroductionNN\HW2_template\cat_dog\\dc_227_t.h5", "w")
 
-for file in listdir(train_dir):
+for file in listdir(test_dir):
+    photo = load_img(test_dir + file, target_size=(227,227))    
+    photo = img_to_array(photo)
+    photo /= 255
+    test_img.append(photo)
+    # train_labels.append(classes)
+
+"""for file in listdir(train_dir):
     classes = 0.0
     if file.startswith("cat"):
         classes = 1.0
@@ -24,8 +31,11 @@ for file in listdir(train_dir):
 
     train_img.append(photo)
     train_labels.append(classes)
+"""
+test_img = asarray(test_img)
+f.create_dataset("test_img", data=test_img)
 
-train_img = asarray(train_img)
+"""train_img = asarray(train_img)
 train_labels = asarray(train_labels)
 
 train_img, test_img, train_labels, test_labels = train_test_split(train_img, train_labels, test_size=0.2, random_state=666)
@@ -35,7 +45,7 @@ f.create_dataset("test_img", data=test_img)
 f.create_dataset("train_lab", data=train_labels)
 f.create_dataset("test_lab", data=test_labels)
 f.create_dataset("val_lab", data=val_labels)
-f.create_dataset("val_img", data=val_img)
+f.create_dataset("val_img", data=val_img)"""
 
 """train_img0, train_img1, train_labels0, train_labels1 = train_test_split(train_img, train_labels, test_size=0.5, random_state=1)
 train_img0, test_img0, train_labels0, test_labels0 = train_test_split(train_img0, train_labels0, test_size=0.2, random_state=1)
